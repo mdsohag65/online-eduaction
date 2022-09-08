c5d0de9fdae5236abc488d81a71d8ae60f8796e4
mdsohag65 <sohag92ahmed@gmail.com>
2022-09-08 13:25:39 +0600

    teachers section done

--- js/app.js
+++ js/app.js
@@ -5,7 +5,31 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle("fa-times");
     navbar.classList.toggle('active');
 }
+
+
 var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".teachers-slider", {
     spaceBetween: 20,
     grabCursor: true,
     loop: true,
